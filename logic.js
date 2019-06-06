6e594677211b78e33d59055318b22e61a8d7efc2
Sergey T <sergei.timoshchenko.jsn@gmail.com>
2019-06-06 09:26:24 +0300

    button added

--- logic.js
+++ logic.js
@@ -5,11 +5,23 @@ function onClick() {
   })
 }
 
+function onUseButton(dir) {
+  console.log(dir)
+}
+
 const renderAccount = (dirs) => {
   let div = document.createElement('div')
   dirs.map(dirName => {
     let row = document.createElement('div')
-    row.innerHTML = dirName
+    let button = document.createElement('button')
+    let name = document.createElement('div')
+    name.innerHTML = dirName
+    button.innerHTML = "use"
+    button.setAttribute('onclick', `onUseButton('${dirName}')`)
+    name.setAttribute('style', 'padding-left: 20px')
+    row.appendChild(button)
+    row.appendChild(name)
+    row.setAttribute('style', 'display:flex; margin-top: 20px;')
     div.appendChild(row)
   })
   document.getElementById('accounts').innerHTML = ''
@@ -21,7 +33,7 @@ const fetchDirs = () => {
   return new Promise(resp => {
     get(cmd).then(res => {
       let resultRows = res.split('\n').slice(3, res.split('\n').length - 1).map(r => r.split(' '))
-      let dirs = resultRows.filter(row => row[0] === 'drwxr-xr-x').map(r => r[r.length - 1])
+      let dirs = resultRows.filter(row => row[0] === 'drwxrwxr-x').map(r => r[r.length - 1])
       return resp(dirs)
     })
   })
